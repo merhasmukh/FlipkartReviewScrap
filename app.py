5250d507c1f5b15a6b9e2272ee955ad7461ffdd3
mhgn <mhgn0001@gmail.com>
2023-02-09 20:18:51 +0530

    update on 9 feb 2023

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask,render_template,request
 
 from utils import flipkartScrap,log
 from flask_cors import CORS,cross_origin
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as bs
+import requests
+
 app=Flask(__name__)
 CORS(app)
 
@@ -22,11 +26,64 @@ def main():
 @cross_origin()
 def index():
     if request.method == 'POST':
-        try:
+        # try:
             searchString = request.form['content'].replace(" ","")
-            print(searchString)
-        except Exception as e:
-            log.get_log(str(e))
+            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
+            uClient = uReq(flipkart_url)
+            flipkartPage = uClient.read()
+            uClient.close()
+            flipkart_html = bs(flipkartPage, "html.parser")
+            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            del bigboxes[0:3]
+            box = bigboxes[0]
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            prodRes = requests.get(productLink)
+            prodRes.encoding='utf-8'
+            prod_html = bs(prodRes.text, "html.parser")
+            # print(prod_html)
+            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+
+            filename = searchString + ".csv"
+            fw = open(filename, "w")
+            headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            fw.write(headers)
+            reviews = []
+            for commentbox in commentboxes:
+                try:
+                    #name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+
+                except:
+                    name = 'No Name'
+
+                try:
+                    #rating.encode(encoding='utf-8')
+                    rating = commentbox.div.div.div.div.text
+
+
+                except:
+                    rating = 'No Rating'
+
+                try:
+                    #commentHead.encode(encoding='utf-8')
+                    commentHead = commentbox.div.div.div.p.text
+
+                except:
+                    commentHead = 'No Comment Heading'
+                try:
+                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    #custComment.encode(encoding='utf-8')
+                    custComment = comtag[0].div.text
+                except Exception as e:
+                    print("Exception while creating dictionary: ",e)
+
+                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                          "Comment": custComment}
+                reviews.append(mydict)
+            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
+        # except Exception as e:
+        #     print('The Exception message is: ',e)
+        #     return 'something is wrong'
     else:
         return render_template('index.html')
 
